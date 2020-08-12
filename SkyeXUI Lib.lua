bffd838a54830d97284a7e006076ed086220e6a1
SkyeX9 <68152997+SkyeX9@users.noreply.github.com>
2020-08-12 10:12:03 -1000

    Update SkyeXUI Lib.lua

--- SkyeXUI Lib.lua
+++ SkyeXUI Lib.lua
@@ -165,7 +165,7 @@ function Library:NewTab(name)
     end
 
     if TabVariable == 1 then
-        OptionVariable = OptionVariable + 1.5
+        OptionVariable = OptionVariable + 0.8
         OrderVar = OrderVar + 1
 
         local ContainerLabel = Instance.new("Frame")
