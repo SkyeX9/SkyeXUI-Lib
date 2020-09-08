29d272fd1394f63858a5992e86d537480cd3ed4b
SkyeX9 <68152997+SkyeX9@users.noreply.github.com>
2020-09-07 23:01:55 -1000

    Update UILib.lua

--- UILib.lua
+++ UILib.lua
@@ -448,7 +448,7 @@ local Lib = {}
             if default then
                 location[flag] = default
                 Text_6.Text = default
-                Fill.Size = UDim2.new(0,175/(min +(max - min)) * default,0,6)
+                Fill.Size = UDim2.new(0,170/(min +(max - min)) * default,0,6)
             end
             local MouseDown = nil
             local function MakeChange()
